7193aeb8dec2fec83d8433b059ec032bf7c4cbb5
alternativeEnding <matashkovv@gmail.com>
2026-03-02 04:24:54 +0300

    Update config.py
    commit 2

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-AUTH_KEY='MDE5YmE3ZmUtNTk1MS03OWI3LTg0MTgtZjdmODc0NmJjNjAzOmIyNjQ4OGRhLTk2MGItNDVmZS1iNGJlLTA2ODRhZWEwMzVhYg=='
+AUTH_KEY='<Ключ>'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PDF_FOLDER = os.path.join(BASE_DIR, "data", "pdfs")
